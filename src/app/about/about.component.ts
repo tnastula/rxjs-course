86afe1b1ab7c60a07bd8953eb2bcab8846d03d3b
Tomasz Nastula <tnastula@outlook.com>
2021-03-10 15:21:11 +0100

    Finished lesson 8 (section 1)

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+import { fromEvent, interval, timer } from "rxjs";
 
 @Component({
-  selector: 'about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  selector: "about",
+  templateUrl: "./about.component.html",
+  styleUrls: ["./about.component.css"],
 })
 export class AboutComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    //this.intervalTest();
+    //this.timerTest();
+    //this.fromEventTest();
   }
 
+  fromEventTest(): void {
+    const click$ = fromEvent(document, "click");
+    click$.subscribe((event) => console.log(event));
+  }
+
+  timerTest(): void {
+    const timer$ = timer(3000, 1000);
+    timer$.subscribe((val) => console.log("timer stream => " + val));
+  }
+
+  intervalTest(): void {
+    // Definition of stream (observable). Nothing is emitted until it is subscribed to.
+    const interval$ = interval(1000);
+
+    const observer = {
+      next: (val: number) => console.log("interval stream => " + val),
+      error: err => console.log(err),
+      complete: () => console.log('interval stream => COMPLETED'),
+    };
+    
+    // Stream ends with error or completion. Stream won't emit more after even single error.
+    const subscription = interval$.subscribe(observer);
+
+    setTimeout(() => subscription.unsubscribe(), 5000);
+  }
 }
